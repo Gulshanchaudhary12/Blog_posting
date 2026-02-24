cc2d37cf2529fae59c5c93c776e46db9a890d4b3
Gulshanchaudhary12 <kumar.gulshan.hb@gmail.com>
2026-02-24 22:39:43 +0530

    blog posting

---
+++ Frontend/src/pages/Feed.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+const Feed = () => {
+  const [posts, setPosts] = useState([
+    {
+      _id: "1",
+      image: "https://ik.imagekit.io/yfkphe8z1/image_fQaSGpc28.jpg",
+      caption: "Friends",
+    },
+  ]);
+
+  useEffect(() =>{
+
+    axios.get('http://localhost:3000/posts')
+    .then((res) =>{
+        setPosts(res.data.posts)
+    })
+  },[])
+
+
+  return (
+    <section className="feed-section">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className="post-card">
+            <img src={post.image} alt={post.caption} />
+            <p>{post.caption}</p>
+          </div>
+        ))
+      ) : (
+        <h1>No posts available</h1>
+      )}
+    </section>
+  );
+};
+
+export default Feed;
